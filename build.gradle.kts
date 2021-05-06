1034326aa1ba6a66bf1486677c45f3512c58f6a9
Claudio Altamura <claudio@claudioaltamura.de>
2021-05-06 08:24:10 +0200

    chore: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,40 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-  java
-  idea
-  eclipse
+    java
+    idea
+    eclipse
+    id("com.diffplug.spotless") version "5.12.4"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 repositories {
-  jcenter()
+    mavenCentral()
 }
 
 dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_9
-  targetCompatibility = JavaVersion.VERSION_1_9
+    sourceCompatibility = JavaVersion.VERSION_1_9
+    targetCompatibility = JavaVersion.VERSION_1_9
 }
 
+spotless {
+    java {
+        googleJavaFormat()
+    }
+    kotlinGradle {
+        ktlint()
+    }
+}
 
 val test by tasks.getting(Test::class) {
-  useJUnitPlatform()
-  testLogging {
-    events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
-  }
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+    }
 }
